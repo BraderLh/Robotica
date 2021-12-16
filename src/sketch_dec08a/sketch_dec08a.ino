b58d8085baccf7b6a4ab0e5dc7063fead87f4980
Bryan L.H <blipe@unsa.edu.pe>
2021-12-16 01:40:46 -0500

    Android project and code
    Images and sources.

---
+++ src/sketch_dec08a/sketch_dec08a.ino
@@ -0,0 +1,14 @@
+int led_rojo = 12;
+int led_verde = 11;
+int buzzer = 10;
+int smokeA0 = A5;
+int sens
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
